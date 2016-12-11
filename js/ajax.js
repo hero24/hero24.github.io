96f3830342c8117ae7e850629df95d93fcdb315f
hero24 <hero24@interia.pl>
2016-12-11 15:15:47 +0000

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -5,21 +5,25 @@
 			if(this.readyState === XMLHttpRequest.DONE){
 				var data = JSON.parse(this.responseText);
 				var working_on = document.getElementById("working_on");
+				var linked = [];
 				for(var i = 0;i<data.length;i++){
-					var repoRequest = new XMLHttpRequest();
-					repoRequest.onreadystatechange = function(){
-						if(this.readyState == XMLHttpRequest.DONE){
-							var repoDetails = JSON.parse(this.responseText);
-							console.log(repoDetails);
-							var node = document.createElement('a');
-							node.setAttribute('href',repoDetails.html_url);
-							console.log(node);
-							node.appendChild(document.createTextNode(repoDetails.name));
-							working_on.appendChild(node);
-						}
-					};
-					repoRequest.open('GET',data[i].repo.url,true);
-					repoRequest.send(null);
+					if(!linked.find((data[i].repo.url))){
+						linked.push(data[i].repo.url);
+						var repoRequest = new XMLHttpRequest();
+						repoRequest.onreadystatechange = function(){
+							if(this.readyState == XMLHttpRequest.DONE){
+								var repoDetails = JSON.parse(this.responseText);
+								console.log(repoDetails);
+								var node = document.createElement('a');
+								node.setAttribute('href',repoDetails.html_url);
+								console.log(node);
+								node.appendChild(document.createTextNode(repoDetails.name));
+								working_on.appendChild(node);
+							}
+						};
+						repoRequest.open('GET',data[i].repo.url,true);
+						repoRequest.send(null);
+					}
 				}
 			}
 		};
