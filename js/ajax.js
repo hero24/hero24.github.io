60a87932bb72ad242b846c338940a47b1d38a404
hero24 <hero24@interia.pl>
2016-12-11 13:17:25 +0000

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,10 +1,12 @@
 ;(function(){
-	var httpRequest = new XMLHttpRequest();
-	httpRequest.onreadystatechange = function(){
-		if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200){
-			alert(httpRequest.responseText);
-		}
-	};
-	httpRequest.open('GET', 'https://api.github.com/users/hero24/repos?type=owner', true);
-	httpRequest.send(null);
+	document.addEventListener("DOMContentLoaded",function(){
+		var httpRequest = new XMLHttpRequest();
+		httpRequest.onreadystatechange = function(){
+			if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200){
+				alert(httpRequest.responseText);
+			}
+		};
+		httpRequest.open('GET', 'https://api.github.com/users/hero24/repos?type=owner', true);
+		httpRequest.send(null);
+	},false);
 })();
