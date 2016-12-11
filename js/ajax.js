a8d2a5e4cb2faabe1717152f6d9abbf1524eaaa9
hero24 <hero24@interia.pl>
2016-12-11 12:56:04 +0000

    Create ajax.js

---
+++ js/ajax.js
@@ -0,0 +1,9 @@
+var httpRequest = new XMLHttpRequest();
+httpRequest.onreadystatechange = function(){
+    if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200){
+	alert(httpRequest.responseText);
+    }
+};
+httpRequest.open('GET', 'https://api.github.com/users/hero24/repos?type=owner', true);
+httpRequest.setRequestHeader('Authorization', 'token efd41ce41979f46ba21abff5bf5f03358260adfb');
+httpRequest.send(null);
