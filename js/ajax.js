f8e2f14185e550c62234abdcd679d4c0518892cb
hero24 <hero24@interia.pl>
2016-12-11 13:56:02 +0000

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -4,7 +4,7 @@
 		httpRequest.onreadystatechange = function(){
 			if(this.readyState === XMLHttpRequest.DONE){
 				var data = JSON.parse(this.responseText);
-				document.getElementById("working_on").innerHTML = data[0].repo.url;
+				document.getElementById("working_on").appendChild(document.createTextNode(data[0].repo.url));
 			}
 		};
 		httpRequest.open('GET', 'https://api.github.com/users/hero24/events/public?client_id=e60a7d85f37e798afe66&type=owner', true);
