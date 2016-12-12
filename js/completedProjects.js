35d0f6df5813e37599c6cefdd74175f7af5f7812
hero24 <hero24@interia.pl>
2016-12-12 13:29:04 +0000

    Update completedProjects.js

--- js/completedProjects.js
+++ js/completedProjects.js
@@ -4,8 +4,7 @@
     langColors.onreadystatechange = function(){
       if(this.readyState == XMLHttpRequest.DONE){
         var colors = [];
-        gitLangDetails = /(.*.:.*.*)/.exec(this.responseText);
-        //var gitLangDetails = this.responseText.split('\n');
+        var gitLangDetails = this.responseText.split('\n');
         console.log(gitLangDetails);
         var languageRequest = new XMLHttpRequest();
         languageRequest.onreadystatechange = function(){
@@ -16,8 +15,15 @@
               repoLangs = new XMLHttpRequest();
               repoLangs.onreadystatechange = function(){
                 if(this.readyState == XMLHttpRequest.DONE){
+                  var lang = JSON.parse(this.responseText);
+                  for (var key in lang) {
+                    if (lang.hasOwnProperty(key)) {
+                      console.log(key + " -> " + lang[key]);
+                    }
+                  }
+                  console.log(lang);
                   langs.innerHTML += this.responseText;
-                  langs.innerHTML += langDetails[i].relases_url;
+                  langs.innerHTML += langDetails[i].releases_url;
                 }
               }
               repoLangs.open('GET',langDetails[i].languages_url,true);
