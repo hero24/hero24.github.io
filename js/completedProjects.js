15592bac132b244da57b1f63997376f469aae784
hero24 <hero24@interia.pl>
2016-12-12 13:02:48 +0000

    Update completedProjects.js

--- js/completedProjects.js
+++ js/completedProjects.js
@@ -1,18 +1,27 @@
 ;(function(){
   document.addEventListener("DOMContentLoaded",function(){
-    var languageRequest = new XMLHttpRequest();
-    languageRequest.onreadystatechange = function(){
+    var langColors = new XMLHttpRequest();
+    langColors.onreadystatechange = function(){
       if(this.readyState == XMLHttpRequest.DONE){
-        var langDetails = JSON.parse(this.responseText);
-        for(var i = 0;i< langDetails.length;i++){
-          var langs = document.getElementById('langs');
-          langs.innerHTML += langDetails[i].releases_url;
-          langs.innerHTML += langDetails[i].languages_url;
-        }
+        var colors = [];
+        console.log(this.responseText);
+        var languageRequest = new XMLHttpRequest();
+        languageRequest.onreadystatechange = function(){
+          if(this.readyState == XMLHttpRequest.DONE){
+            var langDetails = JSON.parse(this.responseText);
+            for(var i = 0;i< langDetails.length;i++){
+              var langs = document.getElementById('langs');
+              langs.innerHTML += langDetails[i].releases_url;
+              langs.innerHTML += langDetails[i].languages_url;
+            }
+          }
+        };
+        languageRequest.open('GET','https://api.github.com/users/hero24/repos',true);
+        languageRequest.send(null);
       }
-    };
-    languageRequest.open('GET','https://api.github.com/users/hero24/repos',true);
-    languageRequest.send(null);
+    }
+    langColors.open('GET','https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml',true);
+    langColors.send(null);
   },false);
 })();
 // 
