604b55edc67b6185d231fbaab3333c31b99349d0
hero24 <hero24@interia.pl>
2016-12-12 12:20:28 +0000

    Update completedProjects.js

--- js/completedProjects.js
+++ js/completedProjects.js
@@ -1,3 +1,16 @@
 ;(function(){
+  document.addEventListener("DOMContentLoaded",function(){
+    var languageRequest = new XMLHttpRequest();
+    languageRequest.onreadystatechange = function(){
+      if(this.readyState == XMLHttpRequest.DONE){
+        var langDetails = JSON.parse(this.responseText);
+        var langs = document.getElementById('langs');
+        langs.innerHTML += this.responseText;
+      }
+    };
+    languageRequest.open('GET','https://api.github.com/users/hero24/repos',true);
+    languageRequest.send(null);
+  },false);
+}})();
+// 
 
-})();
