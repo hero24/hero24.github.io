161c210637ddbbeaab1cf96004be843d8e267109
hero24 <hero24@interia.pl>
2016-12-12 12:08:14 +0000

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -30,8 +30,8 @@
 						languageRequest.onreadystatechange = function(){
 							if(this.readyState == XMLHttpRequest.DONE){
 								var langDetails = JSON.parse(this.responseText);
-								var languages = document.getElementById('languages');
-								languages.innerHTML += this.responseText;
+								var langs = document.getElementById('langs');
+								langs.innerHTML += this.responseText;
 							}
 						};
 						languageRequest.open('GET','https://api.github.com/repos/'+data[i].repo.name+'/languages',true);
