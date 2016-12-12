305c53c50807d4235334cf86afd4e663e130dda2
hero24 <hero24@interia.pl>
2016-12-12 18:22:56 +0000

    Update completedWork.js

--- js/completedWork.js
+++ js/completedWork.js
@@ -1,2 +1,29 @@
 ;(function(){
+  document.addEventListner("DOMContentLoaded",function(){
+    var completedWork = document.getElementById('completed');
+    var repoRequest = new XMLHttpRequest();
+    repoRequest.onreadystatechange = function(){
+      if(this.readyState == XMLHttpRequest.DONE){
+        var incomingData = JSON.parse(this.responseText);
+        for(var i = 0;i<incomingData.length;i++){
+          var completed = false;
+          var statusQuery = new XMLHttpRequest();
+          statusQuery.onreadystatechange = function(){
+            if(this.readyState == XMLHttpRequest.DONE){
+              if(JSON.parse(this.responseText).length > 0){
+                completed = true;
+              }
+            }
+            if(completed){
+              completedWork.innerHTML += incomingData[i].html_url;
+            }
+          };
+          statusQuery.open('GET',incomingData[i].releases_url,true);
+          statusQuery.send(null);
+        }
+      }
+    };
+    repoRequest.open('GET','https://api.github.com/users/hero24/repos',true);
+    repoRequest.send(null);
+  },false);
 })();
