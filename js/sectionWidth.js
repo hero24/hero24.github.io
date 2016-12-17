91d81d09a941478019a50fb28bbc40d973a5bdd5
hero24 <hero24@interia.pl>
2016-12-17 15:25:25 +0000

    Update sectionWidth.js

--- js/sectionWidth.js
+++ js/sectionWidth.js
@@ -1,2 +1,10 @@
 ;(function(){
+  var workingOn = document.getElementById('working_on');
+  var completed = document.getElementById('completed');
+  var resources = document.getElementById('resources');
+  var height = (workingOn.clientHeight > completed.clientHeight)? workingOn.clientHeight : completed.clientHeight;
+  var height = (height > resources.clientHeight) ? height : resources.clientHeight;
+  workingOn.style.height = height;
+  completed.style.height = height;
+  resources.style.height = height;
 })();
