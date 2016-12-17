058f7e7ccd19f8778b2d1fead00ab7faa689d79e
hero24 <hero24@interia.pl>
2016-12-17 16:46:03 +0000

    Update sectionHeight.js

--- js/sectionHeight.js
+++ js/sectionHeight.js
@@ -1,17 +1,19 @@
 ;(function(){
   document.addEventListener('RemoteContentLoaded',function(){
-    var workingOn = document.getElementById('working_on');
-    var completed = document.getElementById('completed');
-    var resources = document.getElementById('resources');
-    var height = (workingOn.clientHeight > completed.clientHeight)? workingOn.clientHeight : completed.clientHeight;
-    var height = (height > resources.clientHeight) ? height : resources.clientHeight;
-    workingOn.style.height = height;
-    completed.style.height = height;
-    resources.style.height = height;
-    var height = "height:"+height+"px;";
-    completed.setAttribute("style",height);
-    workingOn.setAttribute("style",height);
-    resources.setAttribute("style",height);
-    console.log(height);
+    if(document.remoteContentLoaded.workingOn && document.remoteContentLoaded.completed){
+      var workingOn = document.getElementById('working_on');
+      var completed = document.getElementById('completed');
+      var resources = document.getElementById('resources');
+      var height = (workingOn.clientHeight > completed.clientHeight)? workingOn.clientHeight : completed.clientHeight;
+      var height = (height > resources.clientHeight) ? height : resources.clientHeight;
+      workingOn.style.height = height;
+      completed.style.height = height;
+      resources.style.height = height;
+      var height = "height:"+height+"px;";
+      completed.setAttribute("style",height);
+      workingOn.setAttribute("style",height);
+      resources.setAttribute("style",height);
+      console.log(height);
+    }
   },false);
 })();
